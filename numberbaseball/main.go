37068e856332ec04a6af80283e50525fa6eecd8f
myungsworld <myungsworld@gmail.com>
2020-12-08 11:12:01 +0900

    [MOD] implemented

--- numberbaseball/main.go
+++ numberbaseball/main.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+type Result struct {
+	strikes int
+	balls   int
+}
+
 func main() {
 	//난수를 만들때 Seed 값을 시간으로 두면 시간은 계속 변하니까 난수 생성가능
 	rand.Seed(time.Now().UnixNano())
@@ -54,7 +59,7 @@ func MakeNumbers() [3]int {
 		}
 
 	}
-	fmt.Println(rst)
+	//fmt.Println(rst)
 	return rst
 }
 
@@ -62,34 +67,80 @@ func InputNumbers() [3]int {
 	// 키보드로부터 0~9 사이의 겹치지 않는 3개 숫자 반환
 	var rst [3]int
 	for {
+		fmt.Println("겹치지 않는 0~9 사이의 숫자 3개를 입력하세요.")
 		var num int
-		_, err := fmt.Scanf("%d", &num)
+		_, err := fmt.Scanf("%d\n", &num)
+
 		if err != nil {
 			fmt.Println("잘못 입력 하셨습니다.")
 			continue
 		}
+		success := true
 		idx := 0
 		for num > 0 {
 			n := num % 10
 			num = num / 10
+
+			duplicated := false
+			for j := 0; j < idx; j++ {
+				if rst[j] == n {
+					duplicated = true
+					break
+				}
+			}
+			if duplicated {
+				fmt.Println("중복된 값이 있습니다. 다시 입력하세요.")
+				success = false
+				break
+			}
+			if idx >= 3 {
+				// fmt.Println("3개 보다 많은 갑을 입력하셨습니다. 다시 입력하세요.")
+				success = false
+				break
+			}
 			rst[idx] = n
 			idx++
 		}
+		if idx < 3 {
+			// fmt.Println("3개의 숫자를 입력하세요.")
+			success = false
+		}
+
+		if !success {
+			continue
+		}
 		break
 	}
+	rst[0], rst[2] = rst[2], rst[0]
+	fmt.Println(rst)
 	return rst
 }
 
-func CompareNumbers(numbers, inputNumbers [3]int) bool {
+func CompareNumbers(numbers, inputNumbers [3]int) Result {
 	// 두개의 숫자 3개 비교해서 결과 반한
-	return true
+	strikes := 0
+	balls := 0
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if numbers[i] == inputNumbers[j] {
+				if i == j {
+					strikes++
+				} else {
+					balls++
+				}
+			}
+		}
+	}
+
+	return Result{strikes, balls}
 }
 
-func PrintResult(result bool) {
-	println(result)
+func PrintResult(result Result) {
+	fmt.Printf("%dS %dB\n", result.strikes, result.balls)
 }
 
-func IsGameEnd(result bool) bool {
+func IsGameEnd(result Result) bool {
 	// 비교 결과가 3스트라이크 인지 확인
-	return result
+	return result.strikes == 3
 }
