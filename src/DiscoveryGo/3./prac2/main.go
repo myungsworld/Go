76c97197c6355974790d9d8ee7fe53fb34865332
myungsworld <myungsworld@gmail.com>
2021-01-26 06:54:00 -0800

    [ADD] slice add [MOD] README

--- src/DiscoveryGo/3./prac2/main.go
+++ src/DiscoveryGo/3./prac2/main.go
@@ -12,8 +12,15 @@ func addSlice(a []int, i int) {
 	fmt.Println(a)
 }
 
+func addSlice2(a []int, i int) {
+	a = append(a, 8)
+	copy(a[i+1:], a[i:])
+	a[i] = 8
+	fmt.Println(a)
+}
+
 func main() {
 	a := []int{1, 2, 3, 4, 5}
 	addSlice(a, 3)
-
+	addSlice2(a, 4)
 }
